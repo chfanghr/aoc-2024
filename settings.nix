413686e4ddcfdca1907ad00832456fa4515d32fb
Hongrui Fang <chfanghr@gmail.com>
2024-12-06 02:38:26 +0800

    fix invalid locale on darwin

--- settings.nix
+++ settings.nix
@@ -5,12 +5,25 @@
     };
   };
 
-  config.perSystem = { config, ... }: {
-    settings.defaultShellHook = ''
-      export LC_CTYPE=C.UTF-8
-      export LC_ALL=C.UTF-8
-      export LANG=C.UTF-8
-      ${config.pre-commit.installationScript}
-    '';
+  config.perSystem = { config, system, ... }: {
+    settings.defaultShellHook =
+      let isDarwin = lib.hasSuffix "darwin" system; in
+      ''
+        ${
+          if isDarwin then
+            ''
+            export LC_CTYPE=UTF-8
+            export LANG=C
+            ''
+          else
+            ''
+            export LC_CTYPE=C.UTF-8
+            export LC_ALL=C.UTF-8
+            export LANG=C.UTF-8
+            ''
+        }
+
+        ${config.pre-commit.installationScript}
+      '';
   };
 }
