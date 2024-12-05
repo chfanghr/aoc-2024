e850416526afc0d368842261cc347cdf8b838678
Hongrui Fang <chfanghr@gmail.com>
2024-12-05 08:54:35 +0800

    day4 pt2 implemented

--- src/day_4.rs
+++ src/day_4.rs
@@ -4,6 +4,7 @@ use nom::Parser;
 #[derive(Debug)]
 pub struct Answer {
     pub part_1: usize,
+    pub part_2: usize,
 }
 
 pub fn solution<'a>(input: &'a str) -> anyhow::Result<Answer> {
@@ -13,7 +14,8 @@ pub fn solution<'a>(input: &'a str) -> anyhow::Result<Answer> {
         .1;
 
     Ok(Answer {
-        part_1: solution::count_xmas(&input),
+        part_1: solution::part_1::count_xmas(&input),
+        part_2: solution::part_2::count_xmas(&input),
     })
 }
 
@@ -40,38 +42,26 @@ mod parser {
 mod solution {
     use guard::guard;
 
-    fn check_position(grid: &Vec<Vec<char>>, position: &(i32, i32), is_char: char) -> bool {
+    fn check_position(grid: &Vec<Vec<char>>, position: (i32, i32), expected_char: char) -> bool {
         let (row_idx, col_idx) = position;
         guard! {
-            let Some(row) = grid.get(*row_idx as usize) else {
+            let Some(row) = grid.get(row_idx as usize) else {
                 return false
             }
         };
         guard! {
-            let Some(this_char) = row.get(*col_idx as usize) else {
+            let Some(this_char) = row.get(col_idx as usize) else {
                 return false
             }
         }
 
-        is_char == *this_char
+        expected_char == *this_char
     }
 
-    fn check_xmas_sequence(
-        grid: &Vec<Vec<char>>,
-        positions: &[(i32 /* row */, i32 /* col */); 4],
-    ) -> bool {
-        let [x_pos, m_pos, a_pos, s_pos] = positions;
-
-        check_position(grid, x_pos, 'X')
-            && check_position(grid, m_pos, 'M')
-            && check_position(grid, a_pos, 'A')
-            && check_position(grid, s_pos, 'S')
-    }
-
-    fn make_xmas_positions(
+    fn make_positions<const C: usize>(
         current_position: (usize, usize),
-        offset: &[(i32 /* row */, i32 /* col */); 4],
-    ) -> [(i32 /* row */, i32 /* col */); 4] {
+        offset: &[(i32 /* row */, i32 /* col */); C],
+    ) -> [(i32 /* row */, i32 /* col */); C] {
         let (current_row_index, current_col_index) = current_position;
         offset.map(|(row_offset, col_offset)| {
             (
@@ -81,38 +71,115 @@ mod solution {
         })
     }
 
-    pub fn count_xmas(grid: &Vec<Vec<char>>) -> usize {
-        let mut count = 0;
-
-        let offsets: [[(i32 /* row */, i32 /* col */); 4]; 8] = [
-            [(0, 0), (0, -1), (0, -2), (0, -3)],    // left
-            [(0, 0), (0, 1), (0, 2), (0, 3)],       // right
-            [(0, 0), (-1, 0), (-2, 0), (-3, 0)],    // up
-            [(0, 0), (1, 0), (2, 0), (3, 0)],       // down
-            [(0, 0), (-1, -1), (-2, -2), (-3, -3)], // upper left
-            [(0, 0), (-1, 1), (-2, 2), (-3, 3)],    // upper right
-            [(0, 0), (1, -1), (2, -2), (3, -3)],    // lower left
-            [(0, 0), (1, 1), (2, 2), (3, 3)],       // lower right
-        ];
-
+    fn for_each_position<F>(grid: &Vec<Vec<char>>, mut f: F)
+    where
+        F: FnMut((usize, usize)),
+    {
         for row_index in 0..grid.len() {
             let row = grid.get(row_index).unwrap();
             for col_index in 0..row.len() {
+                f((row_index, col_index))
+            }
+        }
+    }
+
+    pub mod part_1 {
+        use super::{check_position, make_positions};
+
+        fn check_xmas_sequence(
+            grid: &Vec<Vec<char>>,
+            positions: [(i32 /* row */, i32 /* col */); 4],
+        ) -> bool {
+            let [x_pos, m_pos, a_pos, s_pos] = positions;
+
+            check_position(grid, x_pos, 'X')
+                && check_position(grid, m_pos, 'M')
+                && check_position(grid, a_pos, 'A')
+                && check_position(grid, s_pos, 'S')
+        }
+
+        pub fn count_xmas(grid: &Vec<Vec<char>>) -> usize {
+            let mut count = 0;
+
+            let offsets: [[(i32 /* row */, i32 /* col */); 4]; 8] = [
+                [(0, 0), (0, -1), (0, -2), (0, -3)],    // left
+                [(0, 0), (0, 1), (0, 2), (0, 3)],       // right
+                [(0, 0), (-1, 0), (-2, 0), (-3, 0)],    // up
+                [(0, 0), (1, 0), (2, 0), (3, 0)],       // down
+                [(0, 0), (-1, -1), (-2, -2), (-3, -3)], // upper left
+                [(0, 0), (-1, 1), (-2, 2), (-3, 3)],    // upper right
+                [(0, 0), (1, -1), (2, -2), (3, -3)],    // lower left
+                [(0, 0), (1, 1), (2, 2), (3, 3)],       // lower right
+            ];
+
+            super::for_each_position(grid, |position| {
                 for offset in offsets {
-                    let positions = make_xmas_positions((row_index, col_index), &offset);
-                    if check_xmas_sequence(&grid, &positions) {
+                    let positions = make_positions(position, &offset);
+                    if check_xmas_sequence(&grid, positions) {
                         count += 1
                     }
                 }
-            }
+            });
+
+            count
         }
 
-        count
+        #[test]
+        fn example() {
+            assert_eq!(
+                count_xmas(&super::super::example::intermediate()),
+                super::super::example::output_p_1()
+            );
+        }
     }
 
-    #[test]
-    fn example() {
-        assert_eq!(count_xmas(&super::example::intermediate()), 18);
+    pub mod part_2 {
+        use super::{check_position, make_positions};
+
+        fn check_combination(
+            grid: &Vec<Vec<char>>,
+            positions: [(i32 /* row */, i32 /* col */); 5],
+            combinatation: [char; 5],
+        ) -> bool {
+            positions
+                .into_iter()
+                .zip(combinatation.into_iter())
+                .all(|(position, expected_char)| check_position(grid, position, expected_char))
+        }
+
+        pub fn count_xmas(grid: &Vec<Vec<char>>) -> usize {
+            let mut count: usize = 0;
+
+            let offset: [(i32 /* row */, i32 /* col */); 5] =
+                [(0, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)];
+
+            let combinations: [[char; 5]; 4] = [
+                ['A', 'M', 'M', 'S', 'S'],
+                ['A', 'M', 'S', 'M', 'S'],
+                ['A', 'S', 'S', 'M', 'M'],
+                ['A', 'S', 'M', 'S', 'M'],
+            ];
+
+            super::for_each_position(grid, |position| {
+                for combination in combinations {
+                    let positions = make_positions(position, &offset);
+                    if check_combination(grid, positions, combination) {
+                        count += 1;
+                        break;
+                    }
+                }
+            });
+
+            return count;
+        }
+
+        #[test]
+        fn example() {
+            assert_eq!(
+                count_xmas(&super::super::example::intermediate()),
+                super::super::example::output_p_2()
+            );
+        }
     }
 }
 
@@ -127,5 +194,13 @@ mod example {
             .into_iter()
             .map(Vec::from)
             .collect()
+    }
+
+    pub fn output_p_1() -> usize {
+        18
+    }
+
+    pub fn output_p_2() -> usize {
+        9
     }
 }
